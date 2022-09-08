988f01a9568ff65a75a73d70a90e5ca68fe5a042
aliabdurohman16 <alyabdurohman16@gmail.com>
2022-09-08 13:13:29 +0700

    update route api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', 'API\UserController@login');
+Route::post('register', 'API\UserController@register');
+
+Route::middleware(['middleware' => 'auth:api'], function() {
+    Route::post('details', 'API\UserController@details');
+});
